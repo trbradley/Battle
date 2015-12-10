3f8ac5d9f50410b893cd7d801e2543ae1be3595d
Thomas Bradley <bradley.tom.r@gmail.com>
2015-12-10 17:59:04 +0000

    Added ability to switch turns

--- lib/game.rb
+++ lib/game.rb
@@ -5,14 +5,16 @@ class Game
   def_delegator :player_1, :hit_points, :player_1_hp
   def_delegator :player_2, :hit_points, :player_2_hp
 
-  attr_reader :players
+  attr_reader :players, :turn
 
   def initialize(player_1, player_2)
     @players = [player_1, player_2]
+    @turn = 0
   end
 
   def attack(player)
     player.receive_damage
+    @turn += 1
   end
 
   def player_1
@@ -23,5 +25,13 @@ class Game
     @players[1]
   end
 
+  def next_player
+    @players[player_selector].name
+  end
 
+  private
+
+  def player_selector
+    @turn % players.length
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,8 +2,8 @@ require 'game'
 
 describe Game do
   subject(:game) { described_class.new(player_1, player_2) }
-  let(:player_1) { double :player}
-  let(:player_2) { double :player }
+  let(:player_1) { double :player, name: 'Johnny Cash' }
+  let(:player_2) { double :player, name: 'Bruce Springsteen', receive_damage: true }
 
   it 'returns the names of players participating' do
     expect(game.players).to eq [player_1, player_2]
@@ -21,6 +21,18 @@ describe Game do
     it 'damages the opponent\'s HP' do
       expect(player_2).to receive(:receive_damage)
       game.attack(player_2)
+    end
+  end
+
+  describe '#turn' do
+    it 'increments after each move' do
+      game.attack(player_2)
+      expect(game.turn).to eq 1
+    end
+
+    it 'tells us whose turn it is' do
+      game.attack(player_2)
+      expect(game.next_player).to eq player_2.name
     end
   end
 end
